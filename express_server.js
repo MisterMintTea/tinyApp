db220a98298b712481126ec250a0145d2129eec8
Victor Huynh <victor.h.huynh@gmail.com>
2019-05-08 15:41:15 +0000

    test

--- express_server.js
+++ express_server.js
@@ -6,12 +6,6 @@ var bodyParser = require("body-parser");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// var generateshortURL
-
-// var shortURL = generateShortURL(),
-// urlDatabase[shortURL] =
-// }
-
 var urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
