609ed9e32616cd9b15d62d1fa4950720b32a2272
Victor Huynh <victor.h.huynh@gmail.com>
2019-05-09 20:47:11 +0000

    cookies

--- express_server.js
+++ express_server.js
@@ -1,11 +1,18 @@
+// GLOBAL REQUIREMENTS
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 var bodyParser = require("body-parser");
+var cookieParser = require('cookie-parser');
 
+// APP SET
 app.set("view engine", "ejs");
+
+// APP USE
+app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// DATABASE 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -18,18 +25,28 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
+  let templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+   };
+
+    res.render("urls_new",templateVars);
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+   };
   res.render("urls_index", templateVars);
 });
 
 // New Route
 app.get("/urls/:shortURL", (req, res) => {
   const shortURLParam = req.params.shortURL;
+  // console.log(shortURLParam); // Refreshing the page
   const templateVars = {
+    username: req.cookies["username"],
     shortURL: shortURLParam,
     longURL: urlDatabase[shortURLParam]
   };
@@ -49,6 +66,16 @@ app.get("/u/:shortURL", (req, res) => {
     res.redirect(longURL);
 });
 
+
+//APP.GET LOGIN
+  app.get("/login", (req, res) => {
+    let templateVars = { 
+      urls: urlDatabase,
+      username: req.cookies["username"]
+     };
+    res.render("urls_index",templateVars);
+
+  });
 function generateRandomString() {
     return (
       Math.random()
@@ -78,12 +105,34 @@ app.post("/urls", (req, res) => {
   var shortUrl = generateRandomString();
     urlDatabase[shortUrl] = longURL
   console.log(req.body);
-  res.redirect('/urls/' + shortUrl);
+  res.redirect('/urls/' + shortUrl); 
   //Create new entry in url database
 });
 
+app.post("/urls/:shortURL/edit", (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.longURL;
+  res.redirect("/urls/" + req.params.shortURL)
+});
+
+app.post("/login", (req, res) => {
+  // set cookie name to username
+  let templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+   };
+  let login = req.body.username;
+  res.cookie('username', login);
+  res.redirect('/urls');
+})
+
+app.post("/logout", (req, res) => {
+  
+  res.clearCookie('username');
+  res.redirect('/urls')
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+ 
